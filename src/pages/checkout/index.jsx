79d63756cc1c194476551b5c4c22ea657220a3ea
Ayo Sam <ayotolusam@gmail.com>
2023-03-15 16:30:16 +0100

    Payment

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,13 +1,9 @@
 import Confirmation from '@/components/Confirmation/Confirmation'
-// import Script from 'next/script';
 
 
 const CheckOut = () => {
   return (
-    <>
-      {/* <Script src="https://js.paystack.co/v1/inline.js" strategy='beforeInteractive' /> */}
-      <Confirmation/>
-    </>
+    <Confirmation/>
   )
 }
 
