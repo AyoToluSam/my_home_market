c878769afe1629c66fb8dc1fd8aeb3412fe1dfdb
Ayo Sam <ayotolusam@gmail.com>
2023-03-18 23:32:01 +0100

    Homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
-import { Container } from './HomePageStyles'
-
+import { Container, Search } from './HomePageStyles'
+import {BiSearch} from 'react-icons/bi'
+import { useCart } from '@/contexts/CartContext'
 
 const HomePage = () => {
+
+  const {products} = useCart()
+
   return (
     <Container>
-      <h1>Hello</h1>
+      <h1>Welcome to My Home Market.</h1>
+      <p>Your buying and selling stop for home equipment and appliances.</p>
+      <Search>
+        <input type="text" placeholder='I am looking for...' />
+        <BiSearch className='search' />
+      </Search>
+      <h2>How we operate</h2>
+      <p>We have a smooth and user friendly operating standard, enumerated in steps below:</p>
+      <ul>
+        <li>Seller......</li>
+        <li>Buyer.....</li>
+        <li>Order processing.....</li>
+        <li>Inspection.....</li>
+        <li>Pickup or Delivery.....</li>
+      </ul>
+      <button>Start shopping</button>
+      <h2>Frequently asked questions</h2>
+      <ul>
+        <li>...</li>
+        <li>...</li>
+        <li>...</li>
+        <li>...</li>
+        <li>...</li>
+      </ul>
     </Container>
   )
 }
--- src/components/HomePage/HomePageStyles.js
+++ src/components/HomePage/HomePageStyles.js
@@ -2,4 +2,36 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  button {
+    padding: 0.5rem;
+    border: none;
+    border-radius: 6px;
+    font-weight: bold;
+  }
+`;
+
+export const Search = styled.div`
+  position: relative;
+  
+  input {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 0.5rem 0.8rem;
+  }
+  .search {
+    position: absolute;
+    top: 0.5rem;
+    right: 1rem;
+  }
 `;
