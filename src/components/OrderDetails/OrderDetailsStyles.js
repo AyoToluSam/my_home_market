8d1f248ae3bb878774efd7f7a02b12850f88017b
Ayo Sam <ayotolusam@gmail.com>
2023-03-18 21:43:45 +0100

    Order Processing

---
+++ src/components/OrderDetails/OrderDetailsStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+
+  button {
+    font-weight: bold;
+    padding: 0.5rem 0.8rem;
+    border: none;
+    border-radius: 8px;
+  }
+`;
