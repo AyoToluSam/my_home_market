9f6c8665aa67ed459fcf43124f9ab4305ed08659
Ayo Sam <ayotolusam@gmail.com>
2023-04-24 21:01:17 +0100

    About

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <Container>
         <h1>About</h1>
         <p>Watch this space...</p>
-        <p>Going live soon!!!</p>
+        <p>Going live soon!</p>
       </Container>
     </>
   )
