d84bd91a93b57aa5630e63a010f4574652765ad2
Ayo Sam <ayotolusam@gmail.com>
2023-06-10 18:34:35 +0100

    Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,12 @@
 import { createPortal } from "react-dom";
+import { Overlay, Content } from "./ModalStyles";
 
 const Modal = ({ open, onClose, children }) => {
   if (!open) return null;
   return createPortal(
     <div>
-      <div onClick={onClose}></div>
-      <div>{children}</div>
+      <Overlay onClick={onClose} />
+      <Content>{children}</Content>
     </div>,
     document.getElementById("portal")
   );
